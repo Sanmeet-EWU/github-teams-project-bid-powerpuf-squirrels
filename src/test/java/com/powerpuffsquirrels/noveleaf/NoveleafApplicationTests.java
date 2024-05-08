5be81c3c5ff8c6c5577dc2f53a3ad5823477d7c2
Reed Hiland <whiland@ewu.edu>
2024-05-08 12:41:55 -0700

    Fixed database connection
    Should be able to run NoveleafApplication with no errors now.

---
+++ src/test/java/com/powerpuffsquirrels/noveleaf/NoveleafApplicationTests.java
@@ -0,0 +1,13 @@
+package com.powerpuffsquirrels.noveleaf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoveleafApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
