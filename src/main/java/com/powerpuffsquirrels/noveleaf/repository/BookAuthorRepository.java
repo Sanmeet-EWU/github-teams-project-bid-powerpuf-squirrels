38bd9dde0f669f0803277c9bfa5aba9a89c41c85
David Leap <dleap@ewu.edu>
2024-05-16 10:30:11 -0700

    Added BookAuthor and BookAuthorRepository

---
+++ src/main/java/com/powerpuffsquirrels/noveleaf/repository/BookAuthorRepository.java
@@ -0,0 +1,6 @@
+package com.powerpuffsquirrels.noveleaf.repository;
+
+import com.powerpuffsquirrels.noveleaf.model.BookAuthor;
+import org.springframework.data.jpa.repository.JpaRepository;
+public interface BookAuthorRepository extends JpaRepository<BookAuthor, Long>{
+}
