96849e5ad54f837e84f39765d3c334fee672135a
Breeanna Lang (Johnson) <bjohnson32@ewu.edu>
2024-05-16 10:38:12 -0700

    get mapping added to create account

--- src/main/java/com/powerpuffsquirrels/noveleaf/controller/CreateAccount.java
+++ src/main/java/com/powerpuffsquirrels/noveleaf/controller/CreateAccount.java
@@ -1,12 +1,14 @@
 package com.powerpuffsquirrels.noveleaf.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
+
 import java.util.Scanner;
 
 public class CreateAccount {
 
     public CreateAccount(String newUsername, String newPass) {
     }
-
+    @GetMapping("/create account")
     public static void newUserCred() {
         System.out.println("Create username: ");
         Scanner input = new Scanner(System.in);
