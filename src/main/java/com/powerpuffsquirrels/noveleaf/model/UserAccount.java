18ed97bf556c872a8959035060dd6d1cfc23e247
David Leap <dleap@ewu.edu>
2024-05-16 10:14:46 -0700

    Fixed UserAccount... again

--- src/main/java/com/powerpuffsquirrels/noveleaf/model/UserAccount.java
+++ src/main/java/com/powerpuffsquirrels/noveleaf/model/UserAccount.java
@@ -18,7 +18,7 @@ public class UserAccount {
 
     public UserAccount(){}
 
-    public UserAccount(String username, Integer hash){
+    public UserAccount(String username, String hash){
         this.username = username;
         this.hash = hash;
     }
@@ -35,11 +35,11 @@ public class UserAccount {
         this.username = username;
     }
 
-    public Integer getHash() {
+    public String getHash() {
         return hash;
     }
 
-    public void setHash(Integer hash) {
+    public void setHash(String hash) {
         this.hash = hash;
     }
 
