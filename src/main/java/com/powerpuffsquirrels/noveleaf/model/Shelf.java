011d16b24153c9057467c0388b5667b0cf75cce9
David Leap <davidjleap@hotmail.com>
2024-05-17 14:33:29 -0700

    edited Shelf to match other entities. Handled foreign key for relation.

--- src/main/java/com/powerpuffsquirrels/noveleaf/model/Shelf.java
+++ src/main/java/com/powerpuffsquirrels/noveleaf/model/Shelf.java
@@ -1,45 +1,27 @@
 package com.powerpuffsquirrels.noveleaf.model;
 
 import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Table(name = "shelf")
 public class Shelf {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name="shelf_id")
     private Integer shelfID; // Primary key
 
+    @Setter
     @Column(nullable = false, name="shelf_name")
     private String shelfName;
 
-    @Column(nullable = false, name="user_id")
-    private Integer userID;
+    @ManyToOne
+    @JoinColumn(nullable = false, name="user_id", referencedColumnName = "user_id")
+    private UserAccount userAccount;
 
-    public Shelf(){}
-
-    public Shelf(String shelfName, Integer userID){
-        this.shelfName = shelfName;
-        this.userID = userID;
-    }
-
-    public Integer getShelfID() {
-        return shelfID;
-    }
-
-    public void setShelfName(String shelfName) {
-        this.shelfName = shelfName;
-    }
-
-    public String getShelfName() {
-        return shelfName;
-    }
-
-    public void setUserID(Integer userID) {
-        this.userID = userID;
-    }
-
-    public Integer getUserID() {
-        return userID;
-    }
 }
