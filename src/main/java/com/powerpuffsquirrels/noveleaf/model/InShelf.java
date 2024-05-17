8285100c2ffbd38051474fbe81c730a2037288ea
David Leap <dleap@ewu.edu>
2024-05-17 09:19:40 -0700

    Edited InShelf. Got rid of un-needed methods. Changed class level @Data to @Getter to ensure isbn and shelf_id are immutable. Added @Setter to date_added and rating to allow mutability. Made InShelfID static as it should be. Removed @Data from InShelfID and replaced with @Getter and @EqualsAndHashCode to ensure immutability.

--- src/main/java/com/powerpuffsquirrels/noveleaf/model/InShelf.java
+++ src/main/java/com/powerpuffsquirrels/noveleaf/model/InShelf.java
@@ -5,15 +5,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import lombok.*;
-import org.hibernate.annotations.Type;
-import org.hibernate.type.descriptor.jdbc.DateJdbcType;
-import org.hibernate.type.descriptor.jdbc.TinyIntJdbcType;
 
 import java.io.Serializable;
 import java.util.Date;
 
 @Entity
-@Data
+@Getter
 @NoArgsConstructor
 @AllArgsConstructor (access = AccessLevel.PRIVATE)//I'm assuming your parameterized constructor doesn't set rating on purpose when building hence why this isn't public
 @Builder //defining an AllArgsConstructor is necessary for the builder, IDK why, since it already has access to a no arg constructor and setters
@@ -21,61 +18,29 @@ import java.util.Date;
 @Table(name = "in_shelf")
 public class InShelf {
 
-    @Data @AllArgsConstructor
-    public class InShelfId implements Serializable {
+    @Id
+    @Column(nullable = false)
+    private String isbn;
+
+    @Id
+    @Column(nullable = false, name = "shelf_id")
+    private Integer shelfID;
+
+    @Setter
+    @Column(nullable = false, name = "date_added")
+    private java.sql.Date dateAdded;
+
+    @Setter
+    private byte rating;
+
+    @Getter
+    @EqualsAndHashCode
+    @NoArgsConstructor
+    @AllArgsConstructor
+    public static class InShelfId implements Serializable {
         private String isbn;
         private Integer shelfID;
 
         // Getter and Setter handled by @Data
     }
-
-
-
-    @Column(nullable = false)
-    @Id
-    private String isbn;
-
-
-    @Column(nullable = false, name = "shelf_id")
-    @Id
-    private Integer shelfID;
-
-    @Column(nullable = false, name = "dateAdded")
-    private java.sql.Date dateAdded;
-
-
-    private byte rating;
-
-  //  public InShelf() {}
-
-    public InShelf(String isbn, Integer shelfID, java.sql.Date   dateAdded){
-        this.isbn = isbn;
-        this.shelfID = shelfID;
-        this.dateAdded = dateAdded;
-    }
-
-/*
-    public String getIsbn() {
-        return isbn;
-    }
-
-    public Integer getShelfID() {
-        return shelfID;
-    }
-
-    public java.sql.Date   getDateAdded() {
-        return dateAdded;
-    }
-
-    public byte getRating() {
-        return rating;
-    }
-
-    public void setRating(byte rating) {
-        this.rating = rating;
-    }
-
-    public void setDateAdded(java.sql.Date   dateAdded) {
-        this.dateAdded = dateAdded;
-    }*/
 }
