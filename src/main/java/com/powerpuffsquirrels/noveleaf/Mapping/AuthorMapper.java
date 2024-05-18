818484511c49de755f053918c2c66b63e51f0261
Yonas E Seyoum <yonasseyoum@gmail.com>
2024-05-18 15:49:28 -0700

    Author Mapping .

--- src/main/java/com/powerpuffsquirrels/noveleaf/Mapping/AuthorMapper.java
+++ src/main/java/com/powerpuffsquirrels/noveleaf/Mapping/AuthorMapper.java
@@ -1,6 +1,39 @@
 package com.powerpuffsquirrels.noveleaf.Mapping;
 
+import com.powerpuffsquirrels.noveleaf.DataTransferObj.AuthorDto;
+import com.powerpuffsquirrels.noveleaf.model.Author;
+
 public class AuthorMapper {
+
+        public static AuthorDto mapToAuthorDto(Author author){
+
+                AuthorDto authorDto = new AuthorDto(
+
+                        author.getAuthorID(),
+                        author.getFirstName(),
+                        author.getLastName()
+                     );
+
+                return authorDto;
+
+
+        }
+
+        public static Author mapToAuthor(AuthorDto authorDto)
+        {
+                Author author= new Author(
+
+                        authorDto.getAuthorID(),
+                        authorDto.getFirstName(),
+                        authorDto.getLastName()
+
+                );
+
+
+                return author;
+
+
+        }
 
 
 }
