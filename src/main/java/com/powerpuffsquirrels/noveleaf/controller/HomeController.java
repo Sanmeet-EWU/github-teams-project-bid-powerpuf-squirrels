d3106bc0c75deef64bc11640eb65350a3e9c2235
Reed HIland <whiland@ewu.edu>
2024-05-20 15:40:15 -0700

    Fixed HomeController, added home page
    Added home page and updated security whitelist so you can see it.

--- src/main/java/com/powerpuffsquirrels/noveleaf/controller/HomeController.java
+++ src/main/java/com/powerpuffsquirrels/noveleaf/controller/HomeController.java
@@ -6,10 +6,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class HomeController {
 
-    @GetMapping("/home")
+    @GetMapping("/") // you want to simply bind this to the root. This is equivalent to http://localhost:10480/
         public String home()
     {
-        return "home";          // home.html
+        return "index";// index.html
 
     }
 }
