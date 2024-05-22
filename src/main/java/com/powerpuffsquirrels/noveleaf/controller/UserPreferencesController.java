4cc23217228aaf9dafdc67abc3f1185d39aab977
Breeanna Lang (Johnson) <bjohnson32@ewu.edu>
2024-05-22 12:39:24 -0700

    created controller class for preferences

---
+++ src/main/java/com/powerpuffsquirrels/noveleaf/controller/UserPreferencesController.java
@@ -0,0 +1,13 @@
+package com.powerpuffsquirrels.noveleaf.controller;
+
+public class UserPreferencesController {
+/*
+Preferences:
+Not in create account
+Separate page after you login
+Preferences get stored in the database attached to that user
+Redirect login to preferences page
+
+ */
+
+}
