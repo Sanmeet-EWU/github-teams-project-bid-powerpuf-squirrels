bf66a5bee15c7abc7dab1a67a82e245d5f49463d
Jared-Schimpf <jschimpf@ewu.edu>
2024-05-23 18:46:56 -0700

    I've done it!!!!! Successful filter that requires login before accessing page elements

---
+++ src/main/java/com/powerpuffsquirrels/noveleaf/Filters/LoginConditionFilter.java
@@ -0,0 +1,33 @@
+package com.powerpuffsquirrels.noveleaf.Filters;
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+public class LoginConditionFilter implements Filter {
+
+    private final List<String> whitelist = Arrays.asList("/login","/logout","/create-account");
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        String requestURI = request.getRequestURI();
+
+        boolean isWhitelisted = whitelist.stream().anyMatch(requestURI::startsWith);
+
+        if(!isWhitelisted && request.getSession().getAttribute("user") == null){
+
+            if(requestURI.equals("/")) response.sendRedirect(request.getContextPath()+"/login");
+            else response.sendRedirect(request.getContextPath()+"/login?error=true");
+            return;
+        }
+
+        filterChain.doFilter(request, response);
+
+    }
+}
