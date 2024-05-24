463bd9a39038bc86dfbdc1945dc3ef9f400e97cb
Reed HIland <whiland@ewu.edu>
2024-05-23 20:08:01 -0700

    A big fat filter
    Might want to change the whitefilter into more of a blackfilter.

--- src/main/java/com/powerpuffsquirrels/noveleaf/Filters/LoginConditionFilter.java
+++ src/main/java/com/powerpuffsquirrels/noveleaf/Filters/LoginConditionFilter.java
@@ -9,7 +9,8 @@ import java.util.List;
 
 public class LoginConditionFilter implements Filter {
 
-    private final List<String> whitelist = Arrays.asList("/login","/logout","/create-account");
+    private final List<String> whitelist = Arrays.asList("/login","/logout","/create-account", "/index", "/search",
+            "/header", "/footer", "/styles.css", "/noveleaf-text.png", "/noveleaf-banner-object.png", "noveleafnobook.png", "popup.js", "popup.css", "login.css", "redirect-message.css" );
 
     @Override
     public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
