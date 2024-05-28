ae42255b3c5956962aa927d08b73932a61b0e432
Breeanna Lang (Johnson) <bjohnson32@ewu.edu>
2024-05-28 10:47:55 -0700

    just /preferences

--- src/main/java/com/powerpuffsquirrels/noveleaf/controller/UserPreferencesController.java
+++ src/main/java/com/powerpuffsquirrels/noveleaf/controller/UserPreferencesController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/preferences")
+@RequestMapping("/preferences")
 public class UserPreferencesController {
     @Autowired
     private UserPreferenceService userPreferenceService;
