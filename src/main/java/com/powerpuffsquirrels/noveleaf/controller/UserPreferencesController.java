3b0bca32780fffde77d9bad6deb67b00581e87c6
Breeanna Lang (Johnson) <bjohnson32@ewu.edu>
2024-05-29 23:28:01 -0700

    updated controller

--- src/main/java/com/powerpuffsquirrels/noveleaf/controller/UserPreferencesController.java
+++ src/main/java/com/powerpuffsquirrels/noveleaf/controller/UserPreferencesController.java
@@ -4,28 +4,32 @@ import com.powerpuffsquirrels.noveleaf.DataTransferObj.UserPreferencesDTO;
 import com.powerpuffsquirrels.noveleaf.model.Preference;
 import com.powerpuffsquirrels.noveleaf.service.imp.UserPreferenceService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
-@RestController
-@RequestMapping("/preferences")
+@Controller
 public class UserPreferencesController {
+
     @Autowired
     private UserPreferenceService userPreferenceService;
 
-    @PostMapping("/{user_id}")
-    public ResponseEntity<Preference> saveUserPreferences(@PathVariable("user_id") Integer userId, @RequestBody UserPreferencesDTO preferencesDTO) {
-        Preference savedPreference = userPreferenceService.saveUserPreferences(userId, preferencesDTO.getGenres(), preferencesDTO.getBookTypes(), preferencesDTO.getValue());
-        return new ResponseEntity<>(savedPreference, HttpStatus.CREATED);
+    @GetMapping("/preferences")
+    public String getUserPreferences(@PathVariable("user_id") Integer userId, Model model) {
+        List<Preference> preferences = userPreferenceService.getUserPreferences(userId);
+        model.addAttribute("preferences", preferences);
+        model.addAttribute("userId", userId);
+        model.addAttribute("preferencesDTO", new UserPreferencesDTO());
+        return "preferences"; // This is the name of the Thymeleaf template (preferences.html)
     }
 
-    @GetMapping("/{user_id}")
-    public ResponseEntity<List<Preference>> getUserPreferences(@PathVariable("user_id") Integer userId) {
-        List<Preference> preferences = userPreferenceService.getUserPreferences(userId);
-        return new ResponseEntity<>(preferences, HttpStatus.OK);
+    @PostMapping("/{user_id}")
+    public String saveUserPreferences(@PathVariable("user_id") Integer userId,
+                                      @ModelAttribute UserPreferencesDTO preferencesDTO, Model model) {
+        userPreferenceService.saveUserPreferences(userId, preferencesDTO.getGenres(), preferencesDTO.getBookTypes(), preferencesDTO.getValue());
+        return "redirect:/preferences/" + userId; // Redirect to the user's preferences page
     }
 
 }
